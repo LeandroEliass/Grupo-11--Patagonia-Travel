63e10c27c7bace3a8789143c2ce3c1448de5644d
unknown <gonzalosanmiguel10@gmail.com>
2021-12-15 15:28:52 -0300

    añadida la ruta de Product Detail

--- src/app.js
+++ src/app.js
@@ -4,6 +4,11 @@ const app = express();
 
 app.set("port",3000)
 app.listen(app.get("port"),()=>console.log("Servidor corriendo"))
+
 app.get("/",(req,res)=>res.sendFile(path.resolve(__dirname,"../views/index.html")))
+
 app.use(express.static(path.resolve(__dirname, "../public")))
 app.get("/",(req,res)=>res.sendFile(path.resolve(__dirname,"../views/login.html")))
+
+app.get("/productDetail",(req,res)=>res.sendFile(path.resolve(__dirname,"../views/productDetail.html")))
+
