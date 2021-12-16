6af3d2a5a3cacb94242956fa655fdbe99f24042e
esteban-nico <nico-9999-esteban@hotmail.com>
2021-12-16 19:00:06 -0300

    Modificando rutas app.js cart y detail

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(express.static(path.resolve(__dirname, "../public")))
 app.get("/",(req,res)=>res.sendFile(path.resolve(__dirname,"../views/index.html")))
 app.get("/login.html",(req,res)=>res.sendFile(path.resolve(__dirname,"../views/login.html")))
 app.get("/register.html",(req,res)=>res.sendFile(path.resolve(__dirname,"../views/register.html")))
-app.get("/productDetail",(req,res)=>res.sendFile(path.resolve(__dirname,"../views/productDetail.html")))
-app.get("/productCar",(req,res)=>res.sendFile(path.resolve(__dirname,"../views/productCar.html")))
+app.get("/productDetail.html",(req,res)=>res.sendFile(path.resolve(__dirname,"../views/productDetail.html")))
+app.get("/productCar.html",(req,res)=>res.sendFile(path.resolve(__dirname,"../views/productCar.html")))
 
 
