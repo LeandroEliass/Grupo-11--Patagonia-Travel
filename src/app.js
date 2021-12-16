04622fcfc38a4994a591fb2e58611398399cfb8d
esteban-nico <nico-9999-esteban@hotmail.com>
2021-12-16 12:13:51 -0300

    Agregando register html y su hoja de registro

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.set("port",3000)
-app.listen(app.get("port"),()=>console.log("Servidor corriendo"))
-app.get("/",(req,res)=>res.sendFile(path.resolve(__dirname,"../views/index.html")))
+app.set('port',process.env.PORT || 3000)
+app.listen(app.get('port'), () => {console.log("SERVIDOR CORRIENDO http://localhost:"+app.get('port'))})
 app.use(express.static(path.resolve(__dirname, "../public")))
+
+app.get("/",(req,res)=>res.sendFile(path.resolve(__dirname,"../views/index.html")))
 app.get("/",(req,res)=>res.sendFile(path.resolve(__dirname,"../views/login.html")))
+app.get("/register.html",(req,res)=>res.sendFile(path.resolve(__dirname,"../views/register.html")))
