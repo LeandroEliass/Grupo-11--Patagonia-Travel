9449da83a90c06e720fc95fb8ba97f622f0ebae5
LeandroEliass <jltoconas.cobra@gmail.com>
2021-12-20 20:53:21 -0300

    detalle heroku

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.set('port',process.env.PORT || 3000)
+
 
 app.listen(process.env.PORT || 3000, function () {
     console.log("Servidor corriendo")})
